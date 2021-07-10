92eeb2836046f9f9448e911628d6bcd4980ecbfd
t-taiki0620 <harunakunya@gmail.com>
2021-07-10 09:05:14 +0900

    wordsArrayをソートして縦に表示するようにした。svgを横に並べれてない

--- src/pages/document-similarity-copy/index.js
+++ src/pages/document-similarity-copy/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import * as d3 from "d3";
-
 const App = () => {
   const [data, setData] = useState([]);
   const dataPath = "./data/plot_test_data_25_DB_16_3.json";
@@ -62,7 +61,7 @@ const DrawDendrogram = ({ word }) => {
   const [selectNodeLeaves, setSelectNodeLeaves] = useState([]);
   const [nodeLeavesData, setNodeLeavesData] = useState([]);
   const [distanceThreshold, setDistanceThreshold] = useState(0);
-  const [selectedNode, setSelectedNode] = useState([]);
+  const [wordsArray, setWordsArray] = useState([]);
   const dataPath = `./data/test.json`;
 
   useEffect(() => {
@@ -101,6 +100,8 @@ const DrawDendrogram = ({ word }) => {
   const contentR = 540;
   const contentWidth = contentR * 2;
   const contentHeight = contentR * 2;
+  const wordsBoxWidth = fontSize * 50;
+  const wordsBoxHeight = fontSize * 100;
   const ministriesCol = d3.scaleOrdinal(d3.schemeCategory10);
 
   const margin = {
@@ -144,7 +145,6 @@ const DrawDendrogram = ({ word }) => {
   cluster(root);
   const nodes = root.descendants();
   const links = root.links();
-  console.log(selectedNode);
 
   const radiusScale = d3
     .scaleLog()
@@ -200,107 +200,135 @@ const DrawDendrogram = ({ word }) => {
         </svg>
       </div>
 
-      <div style={{ overflowX: "scroll" }}>
-        <svg
-          width={contentWidth + margin.left + margin.right}
-          height={contentHeight + margin.top + margin.bottom}
-        >
-          <g
-            transform={`translate(${contentWidth / 2 + margin.left}, ${
-              contentHeight / 2 + margin.top
-            })`}
+      <div class="viwes" style={{ overflowX: "scroll" }}>
+        <div id="graph">
+          <svg
+            width={contentWidth + margin.left + margin.right}
+            height={contentHeight + margin.top + margin.bottom}
           >
-            <g>
-              {links.map(({ source, target }) => {
-                const x1 =
-                  Math.cos(source.x) *
-                  radiusScale(source.data.data.distance + 1);
-                const y1 =
-                  Math.sin(source.x) *
-                  radiusScale(source.data.data.distance + 1);
-                const x2 =
-                  Math.cos(target.x) *
-                  radiusScale(target.data.data.distance + 1);
-                const y2 =
-                  Math.sin(target.x) *
-                  radiusScale(target.data.data.distance + 1);
-                const x3 =
-                  Math.cos(target.x) *
-                  radiusScale(source.data.data.distance + 1);
-                const y3 =
-                  Math.sin(target.x) *
-                  radiusScale(source.data.data.distance + 1);
-                const path = d3.path();
-                path.moveTo(x2, y2);
-                path.lineTo(x3, y3);
-                path.arc(
-                  0,
-                  0,
-                  radiusScale(source.data.data.distance + 1),
-                  target.x,
-                  source.x,
-                  Math.floor((source.x - target.x + 2 * Math.PI) / Math.PI) %
-                    2 ===
-                    1
-                );
-                return (
-                  <g key={`${source.data.data.no}:${target.data.data.no}`}>
-                    <path
-                      d={path.toString()}
-                      stroke="black"
-                      fill="none"
-                      style={{ transition: "d 1s" }}
-                    ></path>
-                  </g>
-                );
-              })}
-            </g>
-            <g>
-              {nodes.map((item, i) => {
-                const x =
-                  Math.cos(item.x) * radiusScale(item.data.data.distance + 1);
-                const y =
-                  Math.sin(item.x) * radiusScale(item.data.data.distance + 1);
-                return (
-                  <g
-                    key={item.data.data.no}
-                    style={{ cursor: "pointer" }}
-                    onClick={(event) => {
-                      const keys = ["pke", "tfidf", "okapi"];
-                      const words = {};
-                      for (const node of item.leaves()) {
-                        for (const key of keys) {
-                          for (word of node.data.data[key]) {
-                            if (!(word.word in words)) {
-                              words[word.word] = 0;
+            <g
+              transform={`translate(${contentWidth / 2 + margin.left}, ${
+                contentHeight / 2 + margin.top
+              })`}
+            >
+              <g>
+                {links.map(({ source, target }) => {
+                  const x1 =
+                    Math.cos(source.x) *
+                    radiusScale(source.data.data.distance + 1);
+                  const y1 =
+                    Math.sin(source.x) *
+                    radiusScale(source.data.data.distance + 1);
+                  const x2 =
+                    Math.cos(target.x) *
+                    radiusScale(target.data.data.distance + 1);
+                  const y2 =
+                    Math.sin(target.x) *
+                    radiusScale(target.data.data.distance + 1);
+                  const x3 =
+                    Math.cos(target.x) *
+                    radiusScale(source.data.data.distance + 1);
+                  const y3 =
+                    Math.sin(target.x) *
+                    radiusScale(source.data.data.distance + 1);
+                  const path = d3.path();
+                  path.moveTo(x2, y2);
+                  path.lineTo(x3, y3);
+                  path.arc(
+                    0,
+                    0,
+                    radiusScale(source.data.data.distance + 1),
+                    target.x,
+                    source.x,
+                    Math.floor((source.x - target.x + 2 * Math.PI) / Math.PI) %
+                      2 ===
+                      1
+                  );
+                  return (
+                    <g key={`${source.data.data.no}:${target.data.data.no}`}>
+                      <path
+                        d={path.toString()}
+                        stroke="black"
+                        fill="none"
+                        style={{ transition: "d 1s" }}
+                      ></path>
+                    </g>
+                  );
+                })}
+              </g>
+              <g>
+                {nodes.map((item, i) => {
+                  const x =
+                    Math.cos(item.x) * radiusScale(item.data.data.distance + 1);
+                  const y =
+                    Math.sin(item.x) * radiusScale(item.data.data.distance + 1);
+                  return (
+                    <g
+                      key={item.data.data.no}
+                      style={{ cursor: "pointer" }}
+                      onClick={(event) => {
+                        const keys = ["pke", "tfidf", "okapi"];
+                        const words = {};
+                        for (const node of item.leaves()) {
+                          for (const key of keys) {
+                            for (word of node.data.data[key]) {
+                              if (!(word.word in words)) {
+                                words[word.word] = 0;
+                              }
+                              words[word.word] += word.score;
                             }
-                            words[word.word] += word.score;
                           }
                         }
-                      }
-
-                      const wordsArray = Object.entries(words).map(
-                        ([word, score]) => ({
-                          word,
-                          score,
-                        })
-                      );
-                      console.log(wordsArray);
-                    }}
-                  >
-                    <circle
-                      cx={x}
-                      cy={y}
-                      r={5}
-                      fill="red"
-                      style={{ transition: "cx 1s, cy 1s" }}
-                    ></circle>
-                  </g>
-                );
-              })}
+                        const wordsArray = Object.entries(words).map(
+                          ([word, score]) => ({
+                            word,
+                            score,
+                          })
+                        );
+                        setWordsArray(
+                          wordsArray.sort(function (a, b) {
+                            if (a.score < b.score) return 1;
+                            if (a.score > b.score) return -1;
+                            return 0;
+                          })
+                        );
+                      }}
+                    >
+                      <circle
+                        cx={x}
+                        cy={y}
+                        r={5}
+                        fill="red"
+                        style={{ transition: "cx 1s, cy 1s" }}
+                      ></circle>
+                    </g>
+                  );
+                })}
+              </g>
             </g>
-          </g>
-        </svg>
+          </svg>
+        </div>
+        <div id="words" margin-left="1405">
+          <svg
+            width={wordsBoxWidth}
+            height={20 * wordsArray.length + margin.top + margin.bottom}
+          >
+            {wordsArray.sort().map((item, i) => {
+              return (
+                <g
+                  transform={`translate(${margin.left / 2}, ${
+                    margin.top + i * 20
+                  })`}
+                >
+                  {console.log(item)}
+                  <text x="7" y="5">
+                    {item.word}
+                  </text>
+                </g>
+              );
+            })}
+          </svg>
+        </div>
       </div>
     </div>
   );
